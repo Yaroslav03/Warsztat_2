daf7b400e0c64ac512c10450875afa55056124f2
Yaroslav <46243946+Yaroslav03@users.noreply.github.com>
2025-08-23 20:43:46 +0200

    Add SearchTextBox in Archive

--- Warsztat_2/UserControls/BarMenu/Archive/UC_Archive.Designer.cs
+++ Warsztat_2/UserControls/BarMenu/Archive/UC_Archive.Designer.cs
@@ -32,6 +32,11 @@
             DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             ViewActualData = new DataGridView();
+            panelDodatkowy = new Panel();
+            panel2 = new Panel();
+            SearchTextBox = new TextBox();
+            label1 = new Label();
+            OrderButton = new Button();
             ID_Column = new DataGridViewTextBoxColumn();
             BtnDelete = new DataGridViewButtonColumn();
             BtnRecover = new DataGridViewButtonColumn();
@@ -42,14 +47,13 @@
             Telefon_Column = new DataGridViewTextBoxColumn();
             Marka_Column = new DataGridViewTextBoxColumn();
             Model_Column = new DataGridViewTextBoxColumn();
-            Column7 = new DataGridViewTextBoxColumn();
+            Zlecenie_Column = new DataGridViewTextBoxColumn();
             WykonawaPracy_Column = new DataGridViewTextBoxColumn();
             KosztKońcowy_Column = new DataGridViewTextBoxColumn();
             VIN_Column = new DataGridViewTextBoxColumn();
-            panelDodatkowy = new Panel();
-            OrderButton = new Button();
             ((System.ComponentModel.ISupportInitialize)ViewActualData).BeginInit();
             panelDodatkowy.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // ViewActualData
@@ -69,7 +73,7 @@
             dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
             ViewActualData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             ViewActualData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            ViewActualData.Columns.AddRange(new DataGridViewColumn[] { ID_Column, BtnDelete, BtnRecover, DataPrzyjęcia_Column, DataZamknięciaZlecenia_Column, Imię_Column, Nazwisko_Column, Telefon_Column, Marka_Column, Model_Column, Column7, WykonawaPracy_Column, KosztKońcowy_Column, VIN_Column });
+            ViewActualData.Columns.AddRange(new DataGridViewColumn[] { ID_Column, BtnDelete, BtnRecover, DataPrzyjęcia_Column, DataZamknięciaZlecenia_Column, Imię_Column, Nazwisko_Column, Telefon_Column, Marka_Column, Model_Column, Zlecenie_Column, WykonawaPracy_Column, KosztKońcowy_Column, VIN_Column });
             dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = Color.FromArgb(64, 64, 70);
             dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
@@ -85,6 +89,71 @@
             ViewActualData.Size = new Size(1029, 540);
             ViewActualData.TabIndex = 38;
             ViewActualData.CellContentClick += ViewActualData_CellContentClick;
+            // 
+            // panelDodatkowy
+            // 
+            panelDodatkowy.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panelDodatkowy.BackColor = Color.FromArgb(64, 64, 70);
+            panelDodatkowy.BorderStyle = BorderStyle.FixedSingle;
+            panelDodatkowy.Controls.Add(panel2);
+            panelDodatkowy.Controls.Add(OrderButton);
+            panelDodatkowy.Controls.Add(ViewActualData);
+            panelDodatkowy.ForeColor = Color.WhiteSmoke;
+            panelDodatkowy.Location = new Point(3, 3);
+            panelDodatkowy.Name = "panelDodatkowy";
+            panelDodatkowy.Size = new Size(1037, 609);
+            panelDodatkowy.TabIndex = 39;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(64, 64, 70);
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(SearchTextBox);
+            panel2.Controls.Add(label1);
+            panel2.ForeColor = Color.DimGray;
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 55);
+            panel2.TabIndex = 40;
+            // 
+            // SearchTextBox
+            // 
+            SearchTextBox.BackColor = Color.FromArgb(64, 64, 70);
+            SearchTextBox.CausesValidation = false;
+            SearchTextBox.Cursor = Cursors.IBeam;
+            SearchTextBox.Font = new Font("Arial", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            SearchTextBox.ForeColor = Color.FromArgb(224, 224, 224);
+            SearchTextBox.Location = new Point(5, 20);
+            SearchTextBox.Name = "SearchTextBox";
+            SearchTextBox.Size = new Size(240, 30);
+            SearchTextBox.TabIndex = 26;
+            SearchTextBox.TextChanged += SearchTextBox_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(224, 224, 224);
+            label1.Location = new Point(5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(157, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Wyszukaj za słowami";
+            // 
+            // OrderButton
+            // 
+            OrderButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            OrderButton.BackColor = Color.FromArgb(94, 148, 255);
+            OrderButton.FlatAppearance.BorderSize = 0;
+            OrderButton.FlatStyle = FlatStyle.Flat;
+            OrderButton.ForeColor = Color.LightGray;
+            OrderButton.Location = new Point(877, 36);
+            OrderButton.Name = "OrderButton";
+            OrderButton.Size = new Size(155, 22);
+            OrderButton.TabIndex = 39;
+            OrderButton.Text = "Faktura";
+            OrderButton.UseVisualStyleBackColor = false;
+            OrderButton.Click += OrderButton_Click;
             // 
             // ID_Column
             // 
@@ -145,11 +214,11 @@
             Model_Column.HeaderText = "Model";
             Model_Column.Name = "Model_Column";
             // 
-            // Column7
+            // Zlecenie_Column
             // 
-            Column7.DataPropertyName = "Zlecenie";
-            Column7.HeaderText = "Zlecenie";
-            Column7.Name = "Column7";
+            Zlecenie_Column.DataPropertyName = "Zlecenie";
+            Zlecenie_Column.HeaderText = "Zlecenie";
+            Zlecenie_Column.Name = "Zlecenie_Column";
             // 
             // WykonawaPracy_Column
             // 
@@ -172,34 +241,6 @@
             VIN_Column.HeaderText = "VIN";
             VIN_Column.Name = "VIN_Column";
             // 
-            // panelDodatkowy
-            // 
-            panelDodatkowy.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            panelDodatkowy.BackColor = Color.FromArgb(64, 64, 70);
-            panelDodatkowy.BorderStyle = BorderStyle.FixedSingle;
-            panelDodatkowy.Controls.Add(OrderButton);
-            panelDodatkowy.Controls.Add(ViewActualData);
-            panelDodatkowy.ForeColor = Color.WhiteSmoke;
-            panelDodatkowy.Location = new Point(3, 3);
-            panelDodatkowy.Name = "panelDodatkowy";
-            panelDodatkowy.Size = new Size(1037, 609);
-            panelDodatkowy.TabIndex = 39;
-            // 
-            // OrderButton
-            // 
-            OrderButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            OrderButton.BackColor = Color.FromArgb(94, 148, 255);
-            OrderButton.FlatAppearance.BorderSize = 0;
-            OrderButton.FlatStyle = FlatStyle.Flat;
-            OrderButton.ForeColor = Color.LightGray;
-            OrderButton.Location = new Point(877, 36);
-            OrderButton.Name = "OrderButton";
-            OrderButton.Size = new Size(155, 22);
-            OrderButton.TabIndex = 39;
-            OrderButton.Text = "Faktura";
-            OrderButton.UseVisualStyleBackColor = false;
-            OrderButton.Click += OrderButton_Click;
-            // 
             // UC_Archive
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -211,6 +252,8 @@
             Load += UC_Archive_Load;
             ((System.ComponentModel.ISupportInitialize)ViewActualData).EndInit();
             panelDodatkowy.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
             }
 
@@ -233,5 +276,9 @@
         private DataGridViewTextBoxColumn WykonawaPracy_Column;
         private DataGridViewTextBoxColumn KosztKońcowy_Column;
         private DataGridViewTextBoxColumn VIN_Column;
+        private Panel panel2;
+        private TextBox SearchTextBox;
+        private Label label1;
+        private DataGridViewTextBoxColumn Zlecenie_Column;
         }
 }
--- Warsztat_2/UserControls/BarMenu/Archive/UC_Archive.cs
+++ Warsztat_2/UserControls/BarMenu/Archive/UC_Archive.cs
@@ -1,7 +1,13 @@
-﻿namespace Warsztat_2.UserControls.BarMenu.Archive {
+﻿using System.Data;
+using System.Windows.Forms;
+using Warsztat_2._0;
+
+namespace Warsztat_2.UserControls.BarMenu.Archive {
     public partial class UC_Archive :UserControl {
         private Guid uniqueKey;
         private readonly string connectionStringArchive = "Data Source=Archive.db;Version=3;New=False;Compress=True;";
+        private readonly BindingSource _bs = new();
+
         public UC_Archive()
             {
             InitializeComponent();
@@ -42,15 +48,16 @@
             if(e.RowIndex < 0 || e.ColumnIndex < 0) return;
 
             string? id = ViewActualData.Rows[e.RowIndex].Cells["ID_Column"].Value.ToString();
-            int selectedIndex = (int)ViewActualData.CurrentRow.Index; 
-            uniqueKey = await SqlCmd.GetUniqueKey(id, "Klienty", "Data Source=Archive.db;Version=3;New=False;Compress=True;"); 
+            int selectedIndex = (int)ViewActualData.CurrentRow.Index;
+            uniqueKey = await SqlCmd.GetUniqueKey(id, "Klienty", "Data Source=Archive.db;Version=3;New=False;Compress=True;");
 
-            if(e.ColumnIndex == ViewActualData.Columns["BtnRecover"].Index && ViewActualData.Rows[e.RowIndex].Cells["ID_Column"].Value != DBNull.Value && id != null) 
+            if(e.ColumnIndex == ViewActualData.Columns["BtnRecover"].Index && ViewActualData.Rows[e.RowIndex].Cells["ID_Column"].Value != DBNull.Value && id != null)
                 {
-                Guid uniqueKey = await SqlCmd.GetUniqueKey(id, "Klienty", "Data Source=Archive.db;Version=3;New=False;Compress=True;"); 
-                bool isSucceed = await SqlCmd.RecoverData(uniqueKey); 
-                if(isSucceed) 
-                    { MessageBox.Show("Dane zostałe przywrócone do domyślnej tablicy danych"); 
+                Guid uniqueKey = await SqlCmd.GetUniqueKey(id, "Klienty", "Data Source=Archive.db;Version=3;New=False;Compress=True;");
+                bool isSucceed = await SqlCmd.RecoverData(uniqueKey);
+                if(isSucceed)
+                    {
+                    MessageBox.Show("Dane zostałe przywrócone do domyślnej tablicy danych");
                     ViewActualData.Rows.RemoveAt(selectedIndex);
                     }
                 }
@@ -62,6 +69,25 @@
                 {
                 GeneretePDF pdf = new();
                 pdf.Create(uniqueKey);
+                }
+            }
+
+        private void SearchTextBox_TextChanged(object sender, EventArgs e)
+            {
+            var dataTable = ViewActualData.DataSource as DataTable;
+            if(dataTable != null)
+                {
+                if(!string.IsNullOrEmpty(SearchTextBox.Text))
+                    {
+                    dataTable.DefaultView.RowFilter = string.Format(
+                        "Imię LIKE '%{0}%' OR Nazwisko LIKE '%{0}%' OR NrTelefonu LIKE '%{0}%' OR Marka LIKE '%{0}%' OR Model LIKE '%{0}%' OR VIN LIKE '%{0}%' OR Zlecenie LIKE '%{0}%'",
+                        SearchTextBox.Text);
+                    Settings.SearchTextBox(SearchTextBox, ViewActualData);
+                    }                
+                else
+                    {
+                    dataTable.DefaultView.RowFilter = string.Empty;
+                    }
                 }
             }
         }
